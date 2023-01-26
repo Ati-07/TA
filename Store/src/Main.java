68c4f5fbf8603714125460a9a8e366aae585e490
Ati Patel <atipatel@Atis-MacBook-Pro.local>
2023-01-25 21:28:11 -0500

    in

---
+++ Store/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    static String storename;
+    public static void main(String[] args){
+        Store store = new Store();
+        Store store = new Store();
+        Store store = new Store();
+        Store.storename = " Ati's Store";
+    }
+
+}
---
+++ Store/src/Store.java
@@ -0,0 +1,8 @@
+public class Store {
+    private String storename;
+
+    public Store () {}
+    public Store (String sname) {
+        storename = sname;
+    }
+}
